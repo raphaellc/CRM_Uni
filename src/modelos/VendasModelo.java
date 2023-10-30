c252ade98a5164e6dc6c24b98922c36c865da1d2
I585296 <I585296@W-PE09WYW7>
2023-10-30 19:57:14 -0300

    - classes criadas na DTO e  Modelo

--- src/modelos/VendasModelo.java
+++ src/modelos/VendasModelo.java
@@ -1,5 +1,55 @@
 package modelos;
 
+import java.util.Date;
+
 public class VendasModelo {
+	
+	private int id_venda;
+	private float valor_venda;
+	private Date dt_venda;
+	
+	// -------------- Getters and Setters ---------------
+	
+	public int getId_venda() {
+		return id_venda;
+	}
+	
+	public void setId_venda(int id_venda) {
+		this.id_venda = id_venda;
+	}
+	
+	public float getValor_venda() {
+		return valor_venda;
+	}
+	
+	public void setValor_venda(float valor_venda) {
+		this.valor_venda = valor_venda;
+	}
+	
+	public Date getDt_venda() {
+		return dt_venda;
+	}
+	
+	public void setDt_venda(Date dt_venda) {
+		this.dt_venda = dt_venda;
+	}
+
+	
+	// --------------- Construtores -------------------
+	
+	public VendasModelo() {
+	}
+	
+	public VendasModelo(int id_venda, float valor_venda, Date dt_venda) {
+		super();
+		this.id_venda = id_venda;
+		this.valor_venda = valor_venda;
+		this.dt_venda = dt_venda;
+	}
+	
+	
+	
+	
+	
 
 }
