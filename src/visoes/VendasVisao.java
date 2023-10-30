be39ef4e1c8196adbbbaa98470ef2824923fc355
Jackson Pandolfo <jacksonpandolfo@hotmail.com>
2023-10-30 20:09:09 -0300

    Criação da tela de vendas

--- src/visoes/VendasVisao.java
+++ src/visoes/VendasVisao.java
@@ -1,5 +1,41 @@
-package visoes;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
 
 public class VendasVisao {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VendasVisao window = new VendasVisao();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VendasVisao() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
