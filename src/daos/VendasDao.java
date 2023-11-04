2f3aa5ce2cb4f30004b600095808df953848800c
Jackson Pandolfo <jacksonpandolfo@hotmail.com>
2023-11-04 20:23:41 -0300

    Estrutura de conexão com o banco de dados

--- src/daos/VendasDao.java
+++ src/daos/VendasDao.java
@@ -1,5 +1,28 @@
 package daos;
 
-public class VendasDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class VendasDao {
+	private Connection con;
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/"; //###############------------------ APÓS A ULTIMA "/" COLOCAR O NOME DO BANCO DE DADOS ------------------###############
+	private String user = ""; //###############------------------ COLOCAR AQUI O USUARIO ------------------###############
+	private String password = ""; //###############------------------ COLOCAR AQUI A SENHA DEFINIDA AO CRIAR O DB ------------------###############
+	
+	public Connection conectar() {
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password); //RESPONSÁVEL POR FAZER A CONEXAO COM O DB
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public void criar() {}
+	public void buscar() {}
+	public void atualizar() {}
+	public void deletar() {}
 }
