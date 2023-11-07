ea64a7be436edf74c91d0b5162a654dec430ce42
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-11-06 21:55:09 -0300

    Campanha DAO e DTO em Dev

---
+++ MarketingDTO/src/Main.java
@@ -0,0 +1,52 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class marketingCampaignDTO() {
+    public static void main(String[] args) {
+            private String campaignName;
+            private String description;
+            private String startDate;
+            private String endDate;
+
+            // Construtores, getters e setters
+
+            public marketingCampaignDTO(String campaignName, String description, String startDate, String endDate) {
+                this.campaignName = campaignName;
+                this.description = description;
+                this.startDate = startDate;
+                this.endDate = endDate;
+            }
+
+            public String getCampaignName() {
+                return campaignName;
+            }
+
+            public void setCampaignName(String campaignName) {
+                this.campaignName = campaignName;
+            }
+
+            public String getDescription() {
+                return description;
+            }
+
+            public void setDescription(String description) {
+                this.description = description;
+            }
+
+            public String getStartDate() {
+                return startDate;
+            }
+
+            public void setStartDate(String startDate) {
+                this.startDate = startDate;
+            }
+
+            public String getEndDate() {
+                return endDate;
+            }
+
+            public void setEndDate(String endDate) {
+                this.endDate = endDate;
+            }
+        }
+
+//-----------------------------------//
