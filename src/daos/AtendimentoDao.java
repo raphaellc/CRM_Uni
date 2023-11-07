a7d3fafac47a60651ba8f0cd65a1fa7ed462ea5c
Victor Ribeiro <127801745+victor-red@users.noreply.github.com>
2023-11-06 21:38:02 -0300

    Correção do AntedimentoDao.

--- src/daos/AtendimentoDao.java
+++ src/daos/AtendimentoDao.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AtendimentoDao {
-    public boolean adicionarAtendimento(Atendimento){
+    public boolean adicionarAtendimento(Atendimento atendimento){
         return true; // Implementar a adição dos atendimentos no banco de dados.
     }
-    public boolean alterarAtendimento(Atendimento){
+    public boolean alterarAtendimento(Atendimento atendimento){
         return true; // Implementar a alteração dos atendimentos no banco de dados.
     }
-    public boolean removerAtendimento(Atendimento){
+    public boolean removerAtendimento(Atendimento atendimento){
         return true; //Implementar a remoção de atendimentos no banco de dados.
     }
     public List<AtendimentoDto> listarAtendimento(){
