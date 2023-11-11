ec09604a73cb13a2d4ef563c9d66817d444632e2
Luga21 <l.gabriel55@hotmail.com>
2023-11-11 19:25:12 -0300

    User adicionado

---
+++ out/production/CRM_Uni/README.md
@@ -0,0 +1,33 @@
+# CRM_Uni
+Aplicação desenvolvida para a Atividade Acadêmica de Implementação de Software - 2023/2
+# Gerência de Configuração de Software
+## Documentação do Projeto
+### Modelos
+https://drive.google.com/file/d/1Mj9ORK-28S2fFJ-rjff1-8mJHN2TSv2Y/view
+### Requisitos
+https://www.moodle.unisinos.br/mod/resource/view.php?id=1694905
+Moodle
+## Ferramentas
+- Oracle Java OpenJDK version 20.0.2
+- MySQL 8.0.34
+- MySQL Server 8.0 2
+- IntelliJ IDEA 2021.3.3 (Ultimate Edition)
+- Build #IU-213.7172.25, built on March 15, 2022
+## Ambientes e Infraestrutura
+- MySQL 8.0
+```
+  CREATE SCHEMA `crmuni` DEFAULT CHARACTER SET utf8 ;
+```
+# Identificação da configuração
+## Métodos de Identificação 
+## Baselines do Projeto
+## Estrutura do Repositório
+- \CRM_Uni
+  - \src
+      - \controladoras
+      - \daos
+      - \dtos
+      - \modelos
+      - \visoes
+## Modelo de Colaboração
+    
--- src/controladora/Usuario.java
+++ src/controladora/Usuario.java
@@ -1,2 +1,35 @@
-package src.controladora;public class Usuario {
+package src.controladora;
+
+public class Usuario {
+        private String nome;
+        private List<ProdutoC> comprasUsuario;
+
+        public Usuario(String nome) {
+            this.nome = nome;
+            this.comprasUsuario = new ArrayList<>();
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public List<Produto> getComprasUsuario() {
+            return comprasUsuario;
+        }
+
+        public void adicionarProduto(Produto produto) {
+            this.comprasUsuario.add(produto);
+        }
+
+        public double calcularTotalCompra() {
+            double total = 0;
+            for (Produto produto : comprasUsuario) {
+                total += produto.getValor();
+            }
+            return total;
+        }
 }
