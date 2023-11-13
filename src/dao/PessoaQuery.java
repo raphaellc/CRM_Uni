201369bd13ee3b04a6d19aed7eb58b962b28b4aa
GregStein2003 <gregorystein10@hotmail.com>
2023-11-13 19:24:01 -0300

    Adicionado models

--- src/dao/PessoaQuery.java
+++ src/dao/PessoaQuery.java
@@ -13,17 +13,34 @@ public interface PessoaQuery {
         + "E.cidade, "
         + "E.estado, "
         + "E.cep "
-        + "FROM Pessoa P "
-        + "LEFT JOIN Setor_Pessoa S ON P.id_setor= S.id_setor_pessoa "
-        + "LEFT JOIN Cargo_Pessoa C on P.id_cargo = C.id_cargo_pessoa "
-        + "LEFT JOIN Endereco E on P.id_pessoa = E.id_pessoa";
+        + "FROM pessoa P "
+        + "LEFT JOIN setor_pessoa S ON P.id_setor= S.id_setor_pessoa "
+        + "LEFT JOIN cargo_pessoa C on P.id_cargo = C.id_cargo_pessoa "
+        + "LEFT JOIN endereco E on P.id_pessoa = E.id_pessoa";
 
 
-    String CRIAR_PESSOA = "INSERT INTO Pessoa(nome, data_nascimento, email, ocupacao, data_hora_origem, celular, id_origem) "
+    String CRIAR_PESSOA = "INSERT INTO pessoa(nome, data_nascimento, email, ocupacao, data_hora_origem, celular, id_origem) "
         + "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    String CRIAR_ENDERECO = "INSERT INTO Endereco(logradouro, numero, complemento, bairro, cidade, estado, cep, id_pessoa) "
+    String CRIAR_ENDERECO = "INSERT INTO endereco(logradouro, numero, complemento, bairro, cidade, estado, cep, id_pessoa) "
         + "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
-    String DELETAR_PESSOA_ID = "DELETE FROM Pessoa WHERE id_pessoa = ?";
+    String CONSULTAR_EMAIL = "SELECT * FROM pessoa WHERE email = ?";
+
+    String CONSULTAR_PESSOA_ID = "SELECT P.*, "
+            + "S.descricao as 'setor', "
+            + "C.descricao as 'cargo', "
+            + "E.id_endereco, "
+            + "E.logradouro, "
+            + "E.numero, "
+            + "E.complemento, "
+            + "E.bairro, "
+            + "E.cidade, "
+            + "E.estado, "
+            + "E.cep "
+            + "FROM pessoa P "
+            + "LEFT JOIN setor_pessoa S ON P.id_setor= S.id_setor_pessoa "
+            + "LEFT JOIN cargo_pessoa C on P.id_cargo = C.id_cargo_pessoa "
+            + "LEFT JOIN endereco E on P.id_pessoa = E.id_pessoa WHERE P.id_pessoa = ?";
+    String DELETAR_PESSOA_ID = "DELETE FROM pessoa WHERE id_pessoa = ?";
 }
