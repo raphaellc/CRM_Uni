f8c06e229153a9785f1477c7fdfcf63cea263164
Bernardo Weiss <bernardo.weiss2@gmail.com>
2023-11-13 21:30:29 -0300

    criação conexão e edcomeço de DAO de atendimento

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -4,19 +4,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = System.getenv("URL");
-    private static final String USUARIO = System.getenv("USUARIO");
-    private static final String SENHA = System.getenv("SENHA");
+    private Connection con;
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String url = System.getenv("url");
+    private static final String username = System.getenv("username");
+    private static final String password = System.getenv("password");
 
-    public static Connection conectar() {
-        Connection connection = null;
+    public Connection conectar() {
 
         try {
-            connection = DriverManager.getConnection(URL, USUARIO, SENHA);
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Conexao estabelecida");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
         }
-
-        return connection;
     }
 }
--- src/dao/DaoRelatorioAtendimento.java
+++ src/dao/DaoRelatorioAtendimento.java
@@ -1,5 +1,31 @@
 package src.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DaoRelatorioAtendimento {
+
+    public static void main(String[] args) {
+        conectar();
+        System.out.println("Hello");
+    }
+
+    public static Connection conectar() {
+        Connection con;
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = System.getenv("url");
+        String username = System.getenv("username");
+        String password = System.getenv("password");
+        try {
+                Class.forName(driver);
+                con = DriverManager.getConnection(url, username, password);
+                System.out.println("Conexao estabelecida");
+                return con;
+        } catch (Exception e) {
+                System.out.println(e);
+                return null;
+        }
+        }
 }
+
