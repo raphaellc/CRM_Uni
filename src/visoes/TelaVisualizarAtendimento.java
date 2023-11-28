8e449790ff6951dfdc31da45a60ae20b16f92714
victor-red <victor_ribeiro01@outlook.com>
2023-11-27 21:48:38 -0300

    Implementação da tela de visualização de atendimentos e adição do método de busca por ID na DAO, correção no método de listagem na DAO.

--- src/visoes/TelaVisualizarAtendimento.java
+++ src/visoes/TelaVisualizarAtendimento.java
@@ -4,92 +4,75 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import daos.AtendimentoDao;
+import dtos.AtendimentoDto;
 
 public class TelaVisualizarAtendimento extends JFrame {
-    //Tela para que o usuário visualize os dados de um atendimento criado, podemos colocar mais campos de visualização
-    //Como por exemplo a prioridade do caso e o id da pessoa responsável, mas isso teremos que ver com o sor.
-    private JTextField txtIdAtendimento;
-    private JTextField txtIdPessoaSolicitante;
-    private JTextField txtStatusAtendimento;
-    private JTextField txtDataAbertura;
-    private JTextArea txtDescricaoProblema;
+
+    private JList<String> listaAtendimentos;
+    private JButton btnVisualizar;
+
+    private AtendimentoDao atendimentoDao;
 
     public TelaVisualizarAtendimento() {
         setTitle("Consulta de Atendimento");
         setSize(400, 450);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panel = new JPanel(new BorderLayout());
 
+        listaAtendimentos = new JList<>();
+        JScrollPane scrollPane = new JScrollPane(listaAtendimentos);
+        panel.add(scrollPane, BorderLayout.CENTER);
 
-        JLabel lblIdAtendimento = new JLabel("ID de Atendimento:");
-        txtIdAtendimento = new JTextField(30);
+        btnVisualizar = new JButton("Visualizar Atendimento");
+        panel.add(btnVisualizar, BorderLayout.SOUTH);
 
-        JButton btnConsultar = new JButton("Consultar Atendimento");
-        JButton btnCancelar = new JButton("Cancelar");
-
-        panel.add(lblIdAtendimento);
-        panel.add(txtIdAtendimento);
-        panel.add(btnConsultar);
-
-
-        //
-        panel.add(new JLabel("Informações do Atendimento:"));
-
-        panel.add(new JLabel("ID de Atendimento:"));
-        txtIdAtendimento = new JTextField(30);
-        txtIdAtendimento.setEditable(false);
-        panel.add(txtIdAtendimento);
-
-        panel.add(new JLabel("ID da Pessoa Solicitante:"));
-        txtIdPessoaSolicitante = new JTextField(30);
-        txtIdPessoaSolicitante.setEditable(false);
-        panel.add(txtIdPessoaSolicitante);
-
-        panel.add(new JLabel("Status do Atendimento:"));
-        txtStatusAtendimento = new JTextField(30);
-        txtStatusAtendimento.setEditable(false);
-        panel.add(txtStatusAtendimento);
-
-        panel.add(new JLabel("Data de Abertura:"));
-        txtDataAbertura = new JTextField(30);
-        txtDataAbertura.setEditable(false);
-        panel.add(txtDataAbertura);
-
-        panel.add(new JLabel("Descrição do Problema:"));
-        txtDescricaoProblema = new JTextArea(4, 30);
-        txtDescricaoProblema.setWrapStyleWord(true);
-        txtDescricaoProblema.setLineWrap(true);
-        txtDescricaoProblema.setCaretPosition(0);
-        txtDescricaoProblema.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(txtDescricaoProblema);
-        panel.add(scrollPane);
-
-        panel.add(btnCancelar);
         add(panel);
 
-        btnConsultar.addActionListener(new ActionListener() {
+        btnVisualizar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Aqui é necessário inserir os métodos para resgatar os dados do atendimento com o ID informado abaixo
-
-                //Comentei essa linha de codigo para testar a interface, já que ao inserir qualquer numero esta dando
-                //um erro que ainda nao consegui arrumar.
-                //int idAtendimento = Integer.parseInt(txtIdAtendimento.getText());
-
-                //Todos os dados resgatados a partir dos métodos das dtos deverão ser passados como parâmetro abaixo
-                txtIdPessoaSolicitante.setText("");
-                txtStatusAtendimento.setText("");
-                txtDataAbertura.setText("");
-                txtDescricaoProblema.setText("");
+                String selectedId = listaAtendimentos.getSelectedValue();
+                if (selectedId != null) {
+                    int idAtendimento = Integer.parseInt(selectedId);
+                    mostrarDetalhesAtendimento(idAtendimento);
+                } else {
+                    JOptionPane.showMessageDialog(TelaVisualizarAtendimento.this, "Selecione um atendimento para visualizar.");
+                }
             }
         });
-        btnCancelar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
-        });
+    }
+
+    public void setAtendimentoDao(AtendimentoDao atendimentoDao) {
+        this.atendimentoDao = atendimentoDao;
+        List<AtendimentoDto> atendimentos = atendimentoDao.listarAtendimento();
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        for (AtendimentoDto atendimentoDto : atendimentos) {
+            listModel.addElement(String.valueOf(atendimentoDto.getIdAtendimento()));
+        }
+        listaAtendimentos.setModel(listModel);
+    }
+
+    private void mostrarDetalhesAtendimento(int idAtendimento) {
+        AtendimentoDto atendimentoSelecionado = atendimentoDao.buscarAtendimentoPorId(idAtendimento);
+
+        JFrame detailsFrame = new JFrame("Detalhes do Atendimento");
+        detailsFrame.setSize(400, 300);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.append("ID de Atendimento: " + atendimentoSelecionado.getIdAtendimento() + "\n");
+        textArea.append("ID da Pessoa Solicitante: " + atendimentoSelecionado.getIdPessoa() + "\n");
+        textArea.append("Status do Atendimento: " + atendimentoSelecionado.getStatusAtualizacao() + "\n");
+        textArea.append("Data de Abertura: " + atendimentoSelecionado.getDtAbertura() + "\n");
+        textArea.append("Data de Resolução: " + atendimentoSelecionado.getDtResolucao() + "\n");
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        detailsFrame.add(scrollPane);
+
+        detailsFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
@@ -97,6 +80,9 @@ public class TelaVisualizarAtendimento extends JFrame {
             public void run() {
                 TelaVisualizarAtendimento frame = new TelaVisualizarAtendimento();
                 frame.setVisible(true);
+
+                AtendimentoDao atendimentoDao = new AtendimentoDao();
+                frame.setAtendimentoDao(atendimentoDao);
             }
         });
     }
