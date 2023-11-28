0afc003ee97c357ec2601559d04e6efed25a5840
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-11-27 21:00:57 -0300

    Controladra sendo criada

--- src/controladorora/Controladora.java
+++ src/controladorora/Controladora.java
@@ -1,4 +1,54 @@
-package controladorora;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.List;
 
-public class tetse {
+public class GerenteMarketingController {
+    private CampanhaDAO campanhaDAO;
+    private ClienteDAO clienteDAO;
+    // Adicione outros DAOs conforme necessário...
+
+    public GerenteMarketingController() {
+        // Inicialize os DAOs conforme necessário
+        Conexao conexao = new Conexao(); // Crie uma nova instância de Conexao ou reutilize a existente
+        this.campanhaDAO = new CampanhaDAO(conexao);
+        this.clienteDAO = new ClienteDAO(conexao);
+        // Inicialize outros DAOs conforme necessário...
+    }
+
+    // Adicione métodos e lógica de controle conforme necessário...
+}
+    public void criarNovaCampanha(String objetivo, Date dataInicio, Date dataFim, List<ClienteDTO> publicoAlvo, String conteudo) {
+        // 2. O gerente cria uma nova campanha de marketing, definindo objetivos e datas de execução.
+        CampanhaDTO novaCampanha = new CampanhaDTO();
+        novaCampanha.setObjetivo(objetivo);
+        novaCampanha.setDataInicio(dataInicio);
+        novaCampanha.setDataFim(dataFim);
+
+        // 3. O gerente seleciona o público-alvo com base em critérios específicos.
+        novaCampanha.setPublicoAlvo(publicoAlvo);
+
+        // 4. O gerente cria conteúdo de campanha, como e-mails, anúncios ou materiais promocionais.
+        novaCampanha.setConteudo(conteudo);
+
+        // 5. A campanha é executada e rastreada em tempo real.
+        campanhaDAO.inserirCampanha(novaCampanha);
+        // Lógica adicional para executar e rastrear a campanha em tempo real, se necessário.
+    }
+
+    public List<CampanhaDTO> buscarTodasCampanhas() {
+        // Método para buscar todas as campanhas, útil para análise de resultados (passo 6).
+        return campanhaDAO.buscarTodasCampanhas();
+    }
+
+    public void analisarResultadosCampanha(int idCampanha) {
+        // 6. O gerente analisa os resultados da campanha, incluindo taxas de abertura, cliques e conversões.
+        CampanhaDTO campanha = campanhaDAO.buscarCampanhaPorId(idCampanha);
+        // Lógica adicional para análise de resultados, por exemplo, exibindo relatórios ou métricas relevantes.
+    }
+
+    // Outros métodos relacionados às ações do gerente de marketing podem ser adicionados conforme necessário.
 }
