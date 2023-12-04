723b4a0846a574584c7298a4432434021c14cc00
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-12-04 19:14:23 -0300

    Atualizado Controladora

--- src/controladorora/Controladora.java
+++ src/controladorora/Controladora.java
@@ -1,25 +1,52 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.List;
 
 public class GerenteMarketingController {
-    private CampanhaDAO campanhaDAO;
-    private ClienteDAO clienteDAO;
-    // Adicione outros DAOs conforme necessário...
+    private CampanhaModelo campanhaModelo;
 
     public GerenteMarketingController() {
-        // Inicialize os DAOs conforme necessário
-        Conexao conexao = new Conexao(); // Crie uma nova instância de Conexao ou reutilize a existente
-        this.campanhaDAO = new CampanhaDAO(conexao);
-        this.clienteDAO = new ClienteDAO(conexao);
-        // Inicialize outros DAOs conforme necessário...
+        this.campanhaModelo = new CampanhaModelo();
     }
 
     public class CampanhaModelo {
+        private List<CampanhaDTO> campanhas;
+        private CampanhaDAO campanhaDAO;
+        private Conexao conexao;
+
+        public CampanhaModelo() {
+            conexao = new Conexao();
+            campanhaDAO = new CampanhaDAO(conexao);
+            campanhas = new ArrayList<>();
+        }
+
+        public void inserirCampanha(CampanhaDTO campanha) {
+            campanhaDAO.inserirCampanha(campanha);
+            campanhas.add(campanha);
+        }
+
+        public List<CampanhaDTO> buscarTodasCampanhas() {
+            return campanhaDAO.buscarTodasCampanhas();
+        }
+
+        public void iniciarCampanha(int campanhaId) {
+            // Lógica para iniciar a execução da campanha em tempo real.
+            // Pode incluir a atualização do status da campanha no banco de dados.
+            // Exemplo: campanhas.get(campanhaId).setStatus("Em Execução");
+        }
+
+        public void encerrarCampanha(int campanhaId) {
+            // Lógica para encerrar a execução da campanha em tempo real.
+            // Pode incluir a atualização do status da campanha no banco de dados.
+            // Exemplo: campanhas.get(campanhaId).setStatus("Encerrada");
+        }
+
+        public void atualizarProgressoCampanha(int campanhaId, int progresso) {
+            // Lógica para atualizar o progresso da campanha em tempo real.
+            // Pode incluir a atualização do progresso no banco de dados.
+            // Exemplo: campanhas.get(campanhaId).setProgresso(progresso);
+        }
+
         public void configurarPublicoAlvo(int campanhaId, List<ClienteDTO> publicoAlvo) {
             // Verifica se a campanha com o ID fornecido existe
             if (campanhaId >= 0 && campanhaId < campanhas.size()) {
@@ -29,74 +56,37 @@ public class GerenteMarketingController {
                 System.out.println("Campanha não encontrada com o ID fornecido.");
             }
         }
+
+        public void criarNovaCampanha(String objetivo, Date dataInicio, Date dataFim,
+                                      List<ClienteDTO> publicoAlvo, String conteudo) {
+            // 2. O gerente cria uma nova campanha de marketing, definindo objetivos e datas de execução.
+            CampanhaDTO novaCampanha = new CampanhaDTO();
+            novaCampanha.setObjetivo(objetivo);
+            novaCampanha.setDataInicio(dataInicio);
+            novaCampanha.setDataFim(dataFim);
+
+            // 3. O gerente seleciona o público-alvo com base em critérios específicos.
+            novaCampanha.setPublicoAlvo(publicoAlvo);
+
+            // 4. O gerente cria conteúdo de campanha, como e-mails, anúncios ou materiais promocionais.
+            novaCampanha.setConteudo(conteudo);
+
+            // 5. A campanha é executada e rastreada em tempo real.
+            campanhaModelo.inserirCampanha(novaCampanha);
+            // Lógica adicional para executar e rastrear a campanha em tempo real, se necessário.
+        }
+
+        public List<CampanhaDTO> buscarTodasCampanhas() {
+            // Método para buscar todas as campanhas, útil para análise de resultados (passo 6).
+            return campanhaModelo.buscarTodasCampanhas();
+        }
+
+        public void analisarResultadosCampanha(int idCampanha) {
+            // 6. O gerente analisa os resultados da campanha, incluindo taxas de abertura, cliques e conversões.
+            CampanhaDTO campanha = campanhaModelo.buscarCampanhaPorId(idCampanha);
+            // Lógica adicional para análise de resultados, por exemplo, exibindo relatórios ou métricas relevantes.
+        }
+
+        // Outros métodos relacionados às ações do gerente de marketing podem ser adicionados conforme necessário.
     }
-}
-    public void criarNovaCampanha(String objetivo, Date dataInicio, Date dataFim, List<ClienteDTO> publicoAlvo, String conteudo) {
-        // 2. O gerente cria uma nova campanha de marketing, definindo objetivos e datas de execução.
-        CampanhaDTO novaCampanha = new CampanhaDTO();
-        novaCampanha.setObjetivo(objetivo);
-        novaCampanha.setDataInicio(dataInicio);
-        novaCampanha.setDataFim(dataFim);
-
-        // 3. O gerente seleciona o público-alvo com base em critérios específicos.
-        novaCampanha.setPublicoAlvo(publicoAlvo);
-
-        // 4. O gerente cria conteúdo de campanha, como e-mails, anúncios ou materiais promocionais.
-        novaCampanha.setConteudo(conteudo);
-
-        // 5. A campanha é executada e rastreada em tempo real.
-        campanhaDAO.inserirCampanha(novaCampanha);
-        // Lógica adicional para executar e rastrear a campanha em tempo real, se necessário.
-    }
-public class CampanhaModelo {
-    private List<CampanhaDTO> campanhas;
-    private CampanhaDAO campanhaDAO;
-    private Conexao conexao;
-
-    public CampanhaModelo() {
-        conexao = new Conexao();
-        campanhaDAO = new CampanhaDAO(conexao);
-        campanhas = new ArrayList<>();
-    }
-
-    public void inserirCampanha(CampanhaDTO campanha) {
-        campanhaDAO.inserirCampanha(campanha);
-        campanhas.add(campanha);
-    }
-
-    public List<CampanhaDTO> buscarTodasCampanhas() {
-        return campanhaDAO.buscarTodasCampanhas();
-    }
-
-    public void iniciarCampanha(int campanhaId) {
-        // Lógica para iniciar a execução da campanha em tempo real.
-        // Pode incluir a atualização do status da campanha no banco de dados.
-        // Exemplo: campanhas.get(campanhaId).setStatus("Em Execução");
-    }
-
-    public void encerrarCampanha(int campanhaId) {
-        // Lógica para encerrar a execução da campanha em tempo real.
-        // Pode incluir a atualização do status da campanha no banco de dados.
-        // Exemplo: campanhas.get(campanhaId).setStatus("Encerrada");
-    }
-
-    public void atualizarProgressoCampanha(int campanhaId, int progresso) {
-        // Lógica para atualizar o progresso da campanha em tempo real.
-        // Pode incluir a atualização do progresso no banco de dados.
-        // Exemplo: campanhas.get(campanhaId).setProgresso(progresso);
-    }
-}
-
-    public List<CampanhaDTO> buscarTodasCampanhas() {
-        // Método para buscar todas as campanhas, útil para análise de resultados (passo 6).
-        return campanhaDAO.buscarTodasCampanhas();
-    }
-
-    public void analisarResultadosCampanha(int idCampanha) {
-        // 6. O gerente analisa os resultados da campanha, incluindo taxas de abertura, cliques e conversões.
-        CampanhaDTO campanha = campanhaDAO.buscarCampanhaPorId(idCampanha);
-        // Lógica adicional para análise de resultados, por exemplo, exibindo relatórios ou métricas relevantes.
-    }
-
-    // Outros métodos relacionados às ações do gerente de marketing podem ser adicionados conforme necessário.
 }
