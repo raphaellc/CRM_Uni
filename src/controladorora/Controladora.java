b4be221ac1a1603fde2c5c68a852d951ae4e50c4
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-12-04 21:01:01 -0300

    Atualizado Controladora

--- src/controladorora/Controladora.java
+++ src/controladorora/Controladora.java
@@ -35,8 +35,60 @@ public class CampanhaModelo {
 
         // 3. O gerente cria uma nova campanha no modelo usando o DTO.
         inserirCampanha(novaCampanhaDTO);
-        // Lógica adicional para executar e rastrear a campanha em tempo real, se necessário.
-    }
+        import dao.CampanhaDAO;
+import dao.StatusPessoaDAO;
+import dto.CampanhaDTO;
+import dto.StatusPessoaDTO;
+import modelo.Campanha;
+import modelo.StatusPessoa;
+
+import java.sql.Connection;
+import java.util.Date;
+import java.util.List;
+
+        public class CampanhaModelo {
+            private final CampanhaDAO campanhaDAO;
+            private final StatusPessoaDAO statusPessoaDAO;
+
+            public CampanhaModelo(Connection connection) {
+                this.campanhaDAO = new CampanhaDAO(connection);
+                this.statusPessoaDAO = new StatusPessoaDAO(connection);
+            }
+
+            public void inserirCampanha(CampanhaDTO campanhaDTO) {
+                campanhaDAO.inserirCampanha(campanhaDTO);
+            }
+
+            public List<CampanhaDTO> buscarTodasCampanhas() {
+                return campanhaDAO.buscarTodasCampanhas();
+            }
+
+            public void criarNovaCampanha(String nome, Date dtInicio, String dtFim) {
+                CampanhaDTO novaCampanhaDTO = new CampanhaDTO();
+                novaCampanhaDTO.setNome(nome);
+                novaCampanhaDTO.setDtInicio(dtInicio);
+                novaCampanhaDTO.setDtFim(dtFim);
+
+
+                inserirCampanha(novaCampanhaDTO);
+                if (!todasCampanhas.isEmpty()) {
+                    int idCampanhaParaRastrear = todasCampanhas.get(0).getIdCampanha(); // Substitua pelo ID real da campanha
+                    gerenteMarketingController.campanhaModelo.iniciarCampanha(idCampanhaParaRastrear);
+
+                }
+
+                public List<StatusPessoaDTO> getAllStatusPessoa() {
+                    return statusPessoaDAO.getAllStatusPessoa();
+                }
+
+                public StatusPessoaDTO getStatusPessoaById(int idStatusPessoa) {
+                    return statusPessoaDAO.getStatusPessoaById(idStatusPessoa);
+                }
+
+                // Outros métodos relacionados às ações do gerente de marketing podem ser adicionados conforme necessário.
+            }
+
+        }
 
     public List<StatusPessoaDTO> getAllStatusPessoa() {
         return statusPessoaDAO.getAllStatusPessoa();
